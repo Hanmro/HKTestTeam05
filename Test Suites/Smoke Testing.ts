8af1a8160d2d41be7e97bcbb38db7ed9da7a85f3
chenxiaozhong <chen.xiaozhong@HKGZPC031.hengkangit.com>
2023-07-31 14:17:23 +0800

    Team leader completes smoke test suite.

---
+++ Test Suites/Smoke Testing.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>chen.xiaozhong@hengkangit.com;</mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>60</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e75cb5a4-ffe0-4c4b-af02-fd42d3ce1b9b</testSuiteGuid>
+   <testCaseLink>
+      <guid>5d1a62b1-86e5-4540-9fb0-37cb43155b59</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b6787111-ff3d-4877-9ad4-531defd23ee2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Registration Test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2574219c-cb81-4794-af57-15e132a7768c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Logout Test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
