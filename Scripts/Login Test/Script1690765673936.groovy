7d6e83a840a5c5141dcd4d4091e2864c2e25f27d
tangzeng <tangzeng@ASJ02.hengkangit.com>
2023-07-31 20:07:09 +0800

    添加备注

--- Scripts/Login Test/Script1690765673936.groovy
+++ Scripts/Login Test/Script1690765673936.groovy
@@ -32,5 +32,5 @@ WebUI.verifyElementText(findTestObject('Object Repository/Login Page/Page_TestPr
 
 WebUI.closeBrowser()
 
-WebUI.comment('zhangrun add the comment')
+WebUI.comment('tangzhenying add the comment')
 
